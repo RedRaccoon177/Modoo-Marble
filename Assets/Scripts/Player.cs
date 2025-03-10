1fee38c463b696ea67e737fa32f65f80d6c5a9d5
Fifow1 <lasldjf123@naver.com>
2025-03-10 17:33:36 +0900

    [Add] 플레이어 칸 이동 기능 구현

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int diceNum;
+    int playerPosIndex;
+    public MapController mapInfo;
+
+    private void Start()
+    {
+        playerPosIndex = 0;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Dice();
+            StartCoroutine(MovePlayer(diceNum));
+        }
+    }
+    public int Dice()
+    {
+        diceNum = Random.Range(1, 7);
+        Debug.Log("주사위 숫자 : " + diceNum);
+        return diceNum;
+    }
+
+    IEnumerator MovePlayer(int num)
+    {
+        int count = 0;
+        Debug.Log("플레이어 현재 위치 인덱스 : " + playerPosIndex);
+        while (count < num)
+        {
+            if ((playerPosIndex + count) >= 31)
+            {
+                playerPosIndex -= 32;
+            }
+            count++;
+            transform.position = mapInfo.grounds[playerPosIndex + count].transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        playerPosIndex += count;
+        mapInfo.GroudInfo(playerPosIndex);
+    }
+}
