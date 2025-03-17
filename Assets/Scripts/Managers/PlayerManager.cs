6e59c84d1f474205cbddddf91224e976a08dd327
Fifow1 <lasldjf123@naver.com>
2025-03-17 14:40:33 +0900

    [Add] 플레이어 이동 중복 처리

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public int userMoney = 0;
     bool isLoan; // 대출여부
-    double _money; // 게임 안에서 사용되는 돈
+    // 게임 안에서 사용되는 돈 , 데이터 베이스 에서 돈을 가져올거임(300만원)
+    double _money; 
     int _mapTurn; // 맵을 몇 바퀴 돌앗는지
-    List<GameObject> _playerGroundLists; // 가지고 있는 건물 리스트
+    List<TileController> _playerGroundLists; // 가지고 있는 건물 리스트
     public void IncreaseMoney(double money)
     {
         _money += money;
@@ -21,11 +24,20 @@ public class PlayerManager : MonoBehaviour
     {
         return _money;
     }
+    public void MapTurn()
+    {
+        _mapTurn++;
+    }
     public void PrintPlayerGroundLists()
     {
         foreach (var item in _playerGroundLists)
         {
             Debug.Log(item);
         }
+    }
+    // 
+    public void AddPlayerGroundLists(TileController tileController)
+    {
+        _playerGroundLists.Add(tileController);
     }
 }
--- Assets/Scripts/Managers/TurnBasedManager.cs
+++ Assets/Scripts/Managers/TurnBasedManager.cs
@@ -6,6 +6,10 @@ public class TurnBasedManager : MonoBehaviour
 {
     int _diceNumFirst;
     int _diceNumSecond;
+    int _currentTurnCount;
+    int _maxTurnCount;
+    // Queue<Player> playerQueue
+    
     public int Dice()
     {
         _diceNumFirst = Random.Range(1, 7);
@@ -13,5 +17,13 @@ public class TurnBasedManager : MonoBehaviour
         Debug.Log("첫 번째 주사위 숫자 : " + _diceNumFirst);
         Debug.Log("두 번째 주사위 숫자 : " + _diceNumSecond);
         return _diceNumFirst + _diceNumSecond;
+    }
+
+    // [PunRpc]
+    public void PlayerTurn()
+    {
+        // playerQueue에서 꺼내고 변수에 저장(현재 차례 플레이어)
+        // playerQueue에서 넣기
+
     }
 }
