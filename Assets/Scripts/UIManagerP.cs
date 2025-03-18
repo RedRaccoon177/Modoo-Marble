0e8def395723d5784315462f1e96fc8cc31ad01d
Fifow1 <lasldjf123@naver.com>
2025-03-18 15:32:53 +0900

    [Fix] 병합 후 오류 해결

--- Assets/Scripts/UIManagerP.cs
+++ Assets/Scripts/UIManagerP.cs
@@ -1,29 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManagerP : MonoBehaviour
 {
+    public static UIManagerP instance;
     [Header("토지 구매 UI")]
     public TileBuyUI _tileGroundBuyUI;
-
-    [Header("관광지 구매 UI")]
+    [Header("광광지 구매 UI")]
     public TileSeaBuyUI _tileSeaBuyUI;
-
-    [Header("타일 UI 생성될 곳")]
+    [Header("타일 UI 생성 될 곳")]
     [SerializeField] Transform _tileParent;
-
     [Header("클릭시 토지 UI")] public GameObject _clickUI; 
+    public event Action<TileController> _clickEvenetGround;
 
 
-    void Start()
+    private void Awake()
     {
+        if (instance == null) 
+        {
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        CreateClickUI();
         TileBuyUI _temp0 = Instantiate(_tileGroundBuyUI, _tileParent);
-        TileSeaBuyUI _temp1 = Instantiate(_tileSeaBuyUI, _tileParent);
+    }
+    #region
+    //public void CreateClickUI()
+    //{
+    //
+    //    for (int i= 0; i < _ClickUI.Length; i ++)
+    //    {
+    //        string key = "";
+    //        if (i == 0) { key = "Ground"; }
+    //        if (i == 1) { key = "Sea"; }
+    //        _tempClickUI[key] = Instantiate(_ClickUI[i]);
+    //    }
+    //}
+    //public void OnClickUI(string key,TileController tileData)
+    //{
+    //    _tempClickUI[key].SetActive(true);
+    //}
+    #endregion
+    public void CreateClickUI()
+    {
+        Instantiate(_clickUI, _tileParent);
     }
 
-    void Update()
+    public void OnPopupGround(TileController _tileController)
     {
-        
+        _clickEvenetGround?.Invoke(_tileController);
     }
 }
