ff2778beabf144a6c860eeaab647898a22245ee1
Fifow1 <lasldjf123@naver.com>
2025-03-18 09:36:27 +0900

    [Refactor] 타일 클릭 이벤트 분리

--- Assets/UIManagerP.cs
+++ Assets/UIManagerP.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UIManagerP : MonoBehaviour
 {
     [Header("토지 UI")] public GameObject _buyUI; 
+    [Header("클릭시 토지 UI")] public GameObject _clickUI; 
 
 
     void Start()
