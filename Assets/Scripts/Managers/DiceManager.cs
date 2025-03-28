6fbcb47ae0790352f3d48148a0f14972995c82a7
Fifow1 <lasldjf123@naver.com>
2025-03-28 16:25:06 +0900

    [Add] 주사위 돌리기

---
+++ Assets/Scripts/Managers/DiceManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviourPun
+{
+    bool isGroundOn;
+    Rigidbody _rb;
+    Coroutine _diceCor;
+    public PhotonView _photonView;
+    public int _diceNum { get; private set; }
+    Quaternion one = Quaternion.Euler(-90, 0, 0);
+    Quaternion two = Quaternion.Euler(0, 0, 0);
+    Quaternion three = Quaternion.Euler(0, 90, -90);
+    Quaternion four = Quaternion.Euler(180, 0, -90);
+    Quaternion five = Quaternion.Euler(180, 0, 0);
+    Quaternion six = Quaternion.Euler(90, 0, 0);
+
+    private void Start()
+    {
+        _photonView = GetComponent<PhotonView>();
+        _rb = GetComponent<Rigidbody>();
+    }
+    [PunRPC]
+    public void DiceStart()
+    {
+        _diceNum = Random.Range(1, 7);
+        _rb.AddForce(0, 10, 0, ForceMode.Impulse);
+        _diceCor = StartCoroutine(RollingDice());
+    }
+
+    IEnumerator RollingDice()
+    {
+        isGroundOn = true;
+        while (isGroundOn == true)
+        {
+            transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "DiceGround")
+        {
+            isGroundOn = false;
+            StopCoroutine(RollingDice());
+            ChangeRotation(_diceNum);
+        }
+    }
+
+    public void ChangeRotation(int num)
+    {
+        if (num == 1) { transform.rotation = one; }
+        else if (num == 2) { transform.rotation = two; }
+        else if (num == 3) { transform.rotation = three; }
+        else if (num == 4) { transform.rotation = four; }
+        else if (num == 5) { transform.rotation = five; }
+        else if (num == 6) { transform.rotation = six; }
+    }
+
+    public int RandomDiceNum()
+    {
+        return _diceNum = Random.Range(1, 7);
+    }
+
+}
--- Assets/Scripts/Managers/TurnBasedManager.cs
+++ Assets/Scripts/Managers/TurnBasedManager.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class TurnBasedManager : MonoBehaviour
 {
     int _diceNumFirst;
     int _diceNumSecond;
-    int _currentTurnCount;
-    int _maxTurnCount;
-    
+
+    public GameObject _redDicePrefab;
+    public GameObject _blueDicePrefab;
+    DiceManager _redDice;
+    DiceManager _blueDice;
+
+    //public int Dice()
+    //{
+    //    _diceNumFirst = Random.Range(1, 7);
+    //    _diceNumSecond = Random.Range(1, 7);
+    //    UIManagerP.instance.InvokeDiceNum(_diceNumFirst, _diceNumSecond);
+    //    return _diceNumFirst + _diceNumSecond;
+    //}
+
+    private void Start()
+    {
+        if (PhotonNetwork.IsMasterClient == true)
+        {
+            _redDice = PhotonNetwork.Instantiate("DiceRed",new Vector3(7,0,-7), Quaternion.identity).GetComponent<DiceManager>();
+            _blueDice = PhotonNetwork.Instantiate("DiceBlue", new Vector3(8, 0, -8), Quaternion.identity).GetComponent<DiceManager>();
+        }
+    }
+
     public int Dice()
     {
-        _diceNumFirst = Random.Range(1, 7);
-        _diceNumSecond = Random.Range(1, 7);
-        UIManagerP.instance.InvokeDiceNum(_diceNumFirst, _diceNumSecond);
-        return _diceNumFirst + _diceNumSecond;
+        _redDice._photonView.RPC("DiceStart", RpcTarget.All);
+        _blueDice._photonView.RPC("DiceStart", RpcTarget.All);
+        return _redDice._diceNum + _blueDice._diceNum;
     }
 }
